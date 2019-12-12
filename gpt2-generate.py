5edb26ba186669dbfda3a16853e17c6505215840
EC2 Default User <ec2-user@ip-172-31-44-23.ap-northeast-1.compute.internal>
2019-12-12 02:12:14 +0000

    Initial Commit

---
+++ gpt2-generate.py
@@ -0,0 +1,95 @@
+import json
+import os
+import numpy as np
+import tensorflow as tf
+import requests
+import argparse
+from tqdm import tqdm
+import MeCab
+from encoder import Encoder
+from model import default_hparams
+from sampling import sample_sequence
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--context', type=str, default='<|endoftext|>')
+parser.add_argument('--num_generate', type=int, default=5)
+args = parser.parse_args()
+
+mecab = MeCab.Tagger("-Owakati")
+
+for filename in ['encoder.json', 'vocab.bpe', 'hparams.json']:
+    if not os.path.isfile(filename):
+        r = requests.get("https://storage.googleapis.com/gpt-2/models/117M/" + filename, stream=True)
+        with open(filename, 'wb') as f:
+            file_size = int(r.headers["content-length"])
+            chunk_size = 1000
+            with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
+                # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    f.write(chunk)
+                    pbar.update(chunk_size)
+
+def get_encoder():
+    with open('encoder.json', 'r') as f:
+        encoder = json.load(f)
+    with open('vocab.bpe', 'r', encoding="utf-8") as f:
+        bpe_data = f.read()
+    bpe_merges = [tuple(merge_str.split()) for merge_str in bpe_data.split('\n')[1:-1]]
+    return Encoder(
+        encoder=encoder,
+        bpe_merges=bpe_merges,
+    )
+
+batch_size=1
+length=None
+temperature=1
+top_k=0
+top_p=0.0
+
+enc = get_encoder()
+hparams = default_hparams()
+with open('hparams.json') as f:
+    hparams.override_from_dict(json.load(f))
+
+if length is None:
+    length = hparams.n_ctx // 2
+elif length > hparams.n_ctx:
+    raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+with tf.Session(graph=tf.Graph()) as sess:
+    context = tf.placeholder(tf.int32, [batch_size, None])
+    output = sample_sequence(
+        hparams=hparams, length=length,
+        context=context,
+        batch_size=batch_size,
+        temperature=temperature, top_k=top_k, top_p=top_p
+    )
+
+    saver = tf.train.Saver()
+    ckpt = tf.train.latest_checkpoint('trained-model-ja-117M/')
+    saver.restore(sess, ckpt)
+
+    generated = 0
+    while True:
+        raw_text = mecab.parse(args.context) if args.context!= '<|endoftext|>' else '<|endoftext|>'
+
+        text = ''
+        while True:
+            context_tokens = enc.encode(raw_text)
+            out = sess.run(output, feed_dict={
+                context: [context_tokens for _ in range(batch_size)]
+            })[:, len(context_tokens):]
+            splitted = enc.decode(out[0]).split('<|endoftext|>')
+
+            text += splitted[0]
+
+            if len(splitted) > 1:
+                break
+            else:
+                raw_text = splitted[0][-256:]
+            print(splitted[0])
+
+        generated += 1
+        if args.num_generate <= generated:
+            break
+        print("="*15)
---
+++ sampling.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+
+import model
+
+def top_k_logits(logits, k):
+    if k == 0:
+        # no truncation
+        return logits
+
+    def _top_k():
+        values, _ = tf.nn.top_k(logits, k=k)
+        min_values = values[:, -1, tf.newaxis]
+        return tf.where(
+            logits < min_values,
+            tf.ones_like(logits, dtype=logits.dtype) * -1e10,
+            logits,
+        )
+    return tf.cond(
+       tf.equal(k, 0),
+       lambda: logits,
+       lambda: _top_k(),
+    )
+
+
+def top_p_logits(logits, p):
+    with tf.variable_scope('top_p_logits'):
+        logits_sort = tf.sort(logits, direction='DESCENDING')
+        probs_sort = tf.nn.softmax(logits_sort)
+        probs_sums = tf.cumsum(probs_sort, axis=1, exclusive=True)
+        logits_masked = tf.where(probs_sums < p, logits_sort, tf.ones_like(logits_sort)*1000) # [batchsize, vocab]
+        min_logits = tf.reduce_min(logits_masked, axis=1, keepdims=True) # [batchsize, 1]
+        return tf.where(
+            logits < min_logits,
+            tf.ones_like(logits, dtype=logits.dtype) * -1e10,
+            logits,
+        )
+
+
+def sample_sequence(*, hparams, length, start_token=None, batch_size=None, context=None, temperature=1, top_k=0, top_p=0.0):
+    if start_token is None:
+        assert context is not None, 'Specify exactly one of start_token and context!'
+    else:
+        assert context is None, 'Specify exactly one of start_token and context!'
+        context = tf.fill([batch_size, 1], start_token)
+
+    def step(hparams, tokens, past=None):
+        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
+
+        logits = lm_output['logits'][:, :, :hparams.n_vocab]
+        presents = lm_output['present']
+        presents.set_shape(model.past_shape(hparams=hparams, batch_size=batch_size))
+        return {
+            'logits': logits,
+            'presents': presents,
+        }
+
+    with tf.name_scope('sample_sequence'):
+        # Don't feed the last context token -- leave that to the loop below
+        # TODO: Would be slightly faster if we called step on the entire context,
+        # rather than leaving the last token transformer calculation to the while loop.
+        context_output = step(hparams, context[:, :-1])
+
+        def body(past, prev, output):
+            next_outputs = step(hparams, prev[:, tf.newaxis], past=past)
+            logits = next_outputs['logits'][:, -1, :]  / tf.to_float(temperature)
+            if top_p > 0.0:
+                logits = top_p_logits(logits, p=top_p)
+            else:
+                logits = top_k_logits(logits, k=top_k)
+            samples = tf.multinomial(logits, num_samples=1, output_dtype=tf.int32)
+            return [
+                tf.concat([past, next_outputs['presents']], axis=-2),
+                tf.squeeze(samples, axis=[1]),
+                tf.concat([output, samples], axis=1),
+            ]
+
+        def cond(*args):
+            return True
+
+        _, _, tokens = tf.while_loop(
+            cond=cond, body=body,
+            maximum_iterations=length,
+            loop_vars=[
+                context_output['presents'],
+                context[:, -1],
+                context,
+            ],
+            shape_invariants=[
+                tf.TensorShape(model.past_shape(hparams=hparams, batch_size=batch_size)),
+                tf.TensorShape([batch_size]),
+                tf.TensorShape([batch_size, None]),
+            ],
+            back_prop=False,
+        )
+
+        return tokens
