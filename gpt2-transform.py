efed85df93e818dc620c1fee69704823df3abd41
tanreinama <tanrei@nama.ne.jp>
2020-08-20 13:15:30 +0900

    add new model

--- gpt2-transform.py
+++ gpt2-transform.py
@@ -7,16 +7,12 @@ import argparse
 from tqdm import tqdm
 import sentencepiece as spm
 from encoder import Encoder
+import model
 from model import default_hparams
-from sampling import sample_sequence
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='ja-117M')
-parser.add_argument('--context', type=str, default='<|endoftext|>')
-parser.add_argument('--num_generate', type=int, default=5)
-parser.add_argument('--top_k', type=int, default=40)
-parser.add_argument('--top_p', type=float, default=0)
-parser.add_argument('--temperature', type=float, default=1)
+parser.add_argument('--context', type=str, required=True)
 args = parser.parse_args()
 
 sp = spm.SentencePieceProcessor()
@@ -52,58 +48,25 @@ def get_encoder():
     )
 
 batch_size=1
-length=None
-temperature=args.temperature
-top_k=args.top_k
-top_p=args.top_p
 
 enc = get_encoder()
 hparams = default_hparams()
 with open(args.model+'/'+'hparams.json') as f:
     hparams.override_from_dict(json.load(f))
 
-if length is None:
-    length = hparams.n_ctx // 2
-elif length > hparams.n_ctx:
-    raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
-
 with tf.Session(graph=tf.Graph()) as sess:
     context = tf.placeholder(tf.int32, [batch_size, None])
-    output = sample_sequence(
-        hparams=hparams, length=length,
-        context=context,
-        batch_size=batch_size,
-        temperature=temperature, top_k=top_k, top_p=top_p
-    )
+    output = model.model(hparams=hparams, X=context[:, :-1], past=None, reuse=tf.AUTO_REUSE)
 
     saver = tf.train.Saver()
     ckpt = tf.train.latest_checkpoint(args.model)
     saver.restore(sess, ckpt)
 
-    generated = 0
-    printed = 0
-    while True:
-        raw_text = sp.EncodeAsPieces(args.context) if args.context!= '<|endoftext|>' else '<|endoftext|>'
-        raw_text = ' '.join([r for r in raw_text if r!='▁'])
-        text = ''
-        while True:
-            context_tokens = enc.encode(raw_text)
-            out = sess.run(output, feed_dict={
-                context: [context_tokens for _ in range(batch_size)]
-            })[:, len(context_tokens):]
-            splitted = enc.decode(out[0]).split('<|endoftext|>')
-
-            text += splitted[0]
-
-            if len(splitted) > 1:
-                break
-            else:
-                raw_text = splitted[0][-256:]
-            print(splitted[0].replace(' ',''))
-            printed += 1
-
-        if printed > 0:
-            generated += 1
-            if args.num_generate <= generated:
-                break
-            print("="*15)
+    raw_text = sp.EncodeAsPieces(args.context)
+    raw_text = ' '.join([r for r in raw_text if r!='▁'])
+    context_tokens = enc.encode(raw_text)
+    out = sess.run(output, feed_dict={
+        context: [context_tokens for _ in range(batch_size)]
+    })
+    output = out['h_flat'][-1]
+    print(output)
